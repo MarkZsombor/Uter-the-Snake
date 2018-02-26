7074c7413c794f592cce579901b7534767a0f40e
Mark Zsombor <markszsombor@gmail.com>
2018-02-25 18:10:09 -0800

    set up pseudo logic

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,21 @@
 var express = require('express')
 var router  = express.Router()
 
+var taunts = [
+  "Don't make me run, I'm full of Chocolate!",
+  "I don't deserve this, I came here legally as an exchange student!",
+  "Oh guten tag. Would you like a bite of my Vengelerstrasse bar?",
+  "I also have a bag of marzipan JoyJoys!",
+  "Would you like another lick of my flavor wax?",
+  "I begged you to look at mine first!"
+];
+
+function getTaunt() {
+  var tauntIndex = Math.floor(Math.random() * (taunts.length));
+  return taunts[tauntIndex];
+}
+
+
 // Handle POST request to '/start'
 router.post('/start', function (req, res) {
   // NOTE: Do something here to start the game
@@ -11,30 +26,29 @@ router.post('/start', function (req, res) {
     name: 'Uter',
     head_type: 'safe',
     tail_type: 'round-bum',
-    taunt: "Don't make me run, I'm full of Chocolate!",
+    taunt: taunts[0],
   }
 
   return res.json(data)
 })
 
+// Need:
+// Limit move choices to non-death choices
+// Find closest food
+// Move to food
+// When close circle food if health is high
+// Eat food if health is low
+
 // Handle POST request to '/move'
 router.post('/move', function (req, res) {
   // NOTE: Do something here to generate your move
 
+
   // Response data
   var data = {
     move: 'up', // one of: ['up','down','left','right']
-    taunt: "I don't deserve this, I came here legally as an exchange student!", // optional, but encouraged!
+    taunt: getTaunt()
   }
-
-  var taunts = [
-    "Don't make me run, I'm full of Chocolate!",
-    "I don't deserve this, I came here legally as an exchange student!",
-    "Oh guten tag. Would you like a bite of my Vengelerstrasse bar?",
-    "I also have a bag of marzipan JoyJoys!",
-    "Would you like another lick of my flavor wax?",
-    "I begged you to look at mine first!"
-  ];
 
   return res.json(data)
 })
