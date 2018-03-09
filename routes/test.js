fddc34b6e6b66f3c77a126f1dd4972785aa548dd
Mark Zsombor <markszsombor@gmail.com>
2018-03-08 18:29:19 -0800

    reorganized test file

--- routes/test.js
+++ routes/test.js
@@ -1,4 +1,5 @@
 //This File is used for testing logic w/o running a server
+//Simply replace the gameState JSON with a /move request JSON to test the server's response to the board.
 
 var PF = require('pathfinding');
 
@@ -544,33 +545,28 @@ const myHead = {
 	y: gameState.you.body.data[0].y
 };
 
-//Determines the distance from the snakes head to something
-const getDistance = (a, b, head) => (Math.abs(a - head.x) + Math.abs(b - head.y));
-
 //Create an empty board
 const grid = new PF.Grid(gameState.width, gameState.height);
 
 //Marks areas on the Grid where the snake can't pass into
 function setGrid(gs, grid) {
 	//Mark my snake in grid
-	for (var i = 1; i < gs.you.body.data.length - 1; i++) {
-		// console.log(gs.you.body.data[i].x, gs.you.body.data[i].y)
+	for (let i = 1; i < gs.you.body.data.length - 1; i++) {
 		grid.setWalkableAt(gs.you.body.data[i].x, gs.you.body.data[i].y, false);
 	}
 	//Mark other snake heads
-	var allSnakes = gs.snakes.data
-	for (var snake in allSnakes) {
-		// console.log(gs.you.id)
+	const allSnakes = gs.snakes.data
+	for (let snake in allSnakes) {
 		if (allSnakes[snake].id !== gs.you.id) {
 			//Don't run into body
 
 			// Account for other snakes length depending on whether they had eaten in the last turn
 			// NOT WORKING
-			// var snakeIndex = allSnakes[snake].body.data.length - 2;
+			// let snakeIndex = allSnakes[snake].body.data.length - 2;
 			// if (allSnakes[snake].body.data[snakeIndex + 1].x == allSnakes[snake].body.data[snakeIndex].x && allSnakes[snake].body.data[snakeIndex + 1].y == allSnakes[snake].body.data[snakeIndex].y) {
 			//   snakeIndex++
 			// }
-			for (var j = 0; j < allSnakes[snake].body.data.length - 1; j++) {
+			for (let j = 0; j < allSnakes[snake].body.data.length - 1; j++) {
 				grid.setWalkableAt(allSnakes[snake].body.data[j].x, allSnakes[snake].body.data[j].y, false);
 			}
 			//Could we run into the head this turn
@@ -597,11 +593,188 @@ function setGrid(gs, grid) {
 	}
 }
 
+// Set the board, choose the target and generate a path
+setGrid(gameState, grid);
+const closestTarget = chooseTarget(gameState);
+const finder = new PF.AStarFinder;
+const path = finder.findPath(myHead.x, myHead.y, closestTarget.x, closestTarget.y, grid);
+const snakeResponse = {};
+
+// if no path exists or a bigger snake can move into the same space choose a safe direction
+if (!path.length || (path.length === 2 && !grid.nodes[path[0][1]][path[0][0]].walkable)) {
+	// console.log('NO PATH')
+	const possibleMoves = [
+		{
+			direction: "right",
+			x: myHead.x + 1,
+			y: myHead.y,
+			valid: true
+		},
+		{
+			direction: "down",
+			x: myHead.x,
+			y: myHead.y + 1,
+			valid: true
+		},
+		{
+			direction: "left",
+			x: myHead.x - 1,
+			y: myHead.y,
+			valid: true
+		},
+		{
+			direction: "up",
+			x: myHead.x,
+			y: myHead.y - 1,
+			valid: true
+		},
+	];
+
+	// Stop the snake from running into itself
+	function checkSelf(gs, pm) {
+		for (let i = 0; i < gs.you.body.data.length - 1; i++) {
+			for (let move in pm) {
+				if (pm[move].x === gs.you.body.data[i].x && pm[move].y === gs.you.body.data[i].y) {
+					pm[move].valid = false;
+				}
+			}
+		}
+	}
+
+	//Stop from running into wall
+	function checkEdges(gs, pm) {
+		for (let move in pm) {
+			if (pm[move].x < 0 || pm[move].x >= gs.width) {
+				pm[move].valid = false;
+			}
+			if (pm[move].y < 0 || pm[move].y >= gs.height) {
+				pm[move].valid = false;
+			}
+		}
+	}
+
+	//check for other snakes
+	function checkSnakes(gs, pm) {
+		const allSnakes = gs.snakes.data
+		for (let snake in allSnakes) {
+			if (allSnakes[snake].id !== gs.you.id) {
+				//Don't run into body
+				for (let i = 0; i < allSnakes[snake].body.data.length - 1; i++) {
+					for (let move in pm) {
+						if (pm[move].x === allSnakes[snake].body.data[i].x && pm[move].y === allSnakes[snake].body.data[i].y) {
+							pm[move].valid = false;
+						}
+					}
+				}
+				//Decide on head collision depending on size
+				if (allSnakes[snake].length >= gs.you.length) {
+					for (let move in pm) {
+						if (pm[move].x === allSnakes[snake].body.data[0].x + 1 && pm[move].y === allSnakes[snake].body.data[0].y) {
+							pm[move].valid = false;
+						}
+						if (pm[move].x === allSnakes[snake].body.data[0].x - 1 && pm[move].y === allSnakes[snake].body.data[0].y) {
+							pm[move].valid = false;
+						}
+						if (pm[move].x === allSnakes[snake].body.data[0].x && pm[move].y === allSnakes[snake].body.data[0].y + 1) {
+							pm[move].valid = false;
+						}
+						if (pm[move].x === allSnakes[snake].body.data[0].x && pm[move].y === allSnakes[snake].body.data[0].y - 1) {
+							pm[move].valid = false;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	checkSelf(gameState, possibleMoves);
+	checkEdges(gameState, possibleMoves);
+	checkSnakes(gameState, possibleMoves);
+
+	const validMoves = [];
+	for (let i in possibleMoves) {
+		if (possibleMoves[i].valid) {
+			validMoves.push(possibleMoves[i]);
+		}
+	}
+
+	// if no spaces are safe, this will allow to move into spaces bigger snakes can allow move into
+	if (!validMoves.length) {
+		// console.log('NO PATH, NO OPEN MOVES');
+
+		//Reset possibleMoves
+		for (let i in possibleMoves) {
+			possibleMoves[i].valid = true
+		}
+
+		//Recheck possibleMoves but ignoring larger snakes
+		gameState.you.length += 100;
+		checkSelf(gameState, possibleMoves);
+		checkEdges(gameState, possibleMoves);
+		checkSnakes(gameState, possibleMoves);
+		for (let i in possibleMoves) {
+			if (possibleMoves[i].valid) {
+				validMoves.push(possibleMoves[i]);
+			}
+		}
+	}
+
+	snakeResponse.move = validMoves[0].direction;
+	snakeResponse.taunt = taunts[1];
+	console.log(snakeResponse);
+	// return res.json(snakeResponse);
+
+} else {
+
+	snakeResponse.move = setMove(path, myHead);
+	snakeResponse.taunt = taunts[getTaunt(gameState)];
+	console.log(snakeResponse);
+	// return res.json(snakeResponse);
+
+}
+
+//Helper functions
+
+//Convert the calculated path coords to a direction of movement
+function setMove(path, head) {
+	if (path[1][0] === head.x && path[1][1] === head.y + 1) {
+		return 'down';
+	} else if (path[1][0] === head.x && path[1][1] === head.y - 1) {
+		return 'up';
+	} else if (path[1][0] === head.x + 1 && path[1][1] === head.y) {
+		return 'right';
+	} else if (path[1][0] === head.x - 1 && path[1][1] === head.y) {
+		return 'left';
+	} else {
+		return 'down';
+	}
+}
+
+// Make Uter say funny things for hilarity
+function getTaunt(gs) {
+	let tauntIndex = 0;
+	if (gs.you.health > 90) {
+		tauntIndex = 0;
+	} else if (gs.you.health < 30) {
+		tauntIndex = 5;
+	} else if (gs.turn < 100) {
+		tauntIndex = 4;
+	} else if (gs.turn < 150) {
+		tauntIndex = 2;
+	} else {
+		tauntIndex = 3;
+	}
+	return tauntIndex;
+}
+
+//Determines the distance from the snakes head to something
+const getDistance = (a, b, head) => (Math.abs(a - head.x) + Math.abs(b - head.y));
+
 //return the closest food item
 function findFood(gs) {
-	var allTargets = [];
-	for (var i in gs.food.data) {
-		var distance = getDistance(gs.food.data[i].x, gs.food.data[i].y, myHead);
+	const allTargets = [];
+	for (let i in gs.food.data) {
+		let distance = getDistance(gs.food.data[i].x, gs.food.data[i].y, myHead);
 		//Add a weight that reduces the likelihood of targeting wall food
 		if (!gs.food.data[i].x || !gs.food.data[i].y || gs.food.data[i].x === gs.width - 1 || gs.food.data[i].y === gs.height - 1) {
 			distance += 10;
@@ -642,9 +815,9 @@ function findTail(gs) {
 
 //Determine the longest snake
 function getLongestLength(gs) {
-	var allSnakes = gs.snakes.data
-	var longestSnake = 0;
-	for (var snake in allSnakes) {
+	const allSnakes = gs.snakes.data
+	let longestSnake = 0;
+	for (let snake in allSnakes) {
 		if (allSnakes[snake].id !== gs.you.id) {
 			if (allSnakes[snake].length > longestSnake) {
 				longestSnake = allSnakes[snake].length;
@@ -669,158 +842,4 @@ function chooseTarget(gs) {
 	} else {
 		return findFood(gs);
 	}
-}
-
-// Set the board, choose the target and generate a path
-setGrid(gameState, grid);
-const closestTarget = chooseTarget(gameState);
-const finder = new PF.AStarFinder;
-const path = finder.findPath(myHead.x, myHead.y, closestTarget.x, closestTarget.y, grid);
-const snakeResponse = {};
-
-// if no path exists or a bigger snake can move into the same space choose a safe direction
-if (!path.length || (path.length === 2 && !grid.nodes[path[0][1]][path[0][0]].walkable)) {
-	// console.log('NO PATH')
-	var possibleMoves = [
-		{
-			direction: "right",
-			x: myHead.x + 1,
-			y: myHead.y,
-			valid: true
-		},
-		{
-			direction: "down",
-			x: myHead.x,
-			y: myHead.y + 1,
-			valid: true
-		},
-		{
-			direction: "left",
-			x: myHead.x - 1,
-			y: myHead.y,
-			valid: true
-		},
-		{
-			direction: "up",
-			x: myHead.x,
-			y: myHead.y - 1,
-			valid: true
-		},
-	];
-
-	// Stop the snake from running into itself
-	function checkSelf(gs, pm) {
-		for (var i = 0; i < gs.you.body.data.length - 1; i++) {
-			for (var move in pm) {
-				if (pm[move].x === gs.you.body.data[i].x && pm[move].y === gs.you.body.data[i].y) {
-					pm[move].valid = false;
-				}
-			}
-		}
-	}
-
-	//Stop from running into wall
-	function checkEdges(gs, pm) {
-		for (var move in pm) {
-			if (pm[move].x < 0 || pm[move].x >= gs.width) {
-				pm[move].valid = false;
-			}
-			if (pm[move].y < 0 || pm[move].y >= gs.height) {
-				pm[move].valid = false;
-			}
-		}
-	}
-
-	//check for other snakes
-	function checkSnakes(gs, pm) {
-		var allSnakes = gs.snakes.data
-		for (var snake in allSnakes) {
-			if (allSnakes[snake].id !== gs.you.id) {
-				//Don't run into body
-				for (var i = 0; i < allSnakes[snake].body.data.length - 1; i++) {
-					for (var move in pm) {
-						if (pm[move].x === allSnakes[snake].body.data[i].x && pm[move].y === allSnakes[snake].body.data[i].y) {
-							pm[move].valid = false;
-						}
-					}
-				}
-				//Decide on head collision depending on size
-				if (allSnakes[snake].length >= gs.you.length) {
-					for (var move in pm) {
-						if (pm[move].x === allSnakes[snake].body.data[0].x + 1 && pm[move].y === allSnakes[snake].body.data[0].y) {
-							pm[move].valid = false;
-						}
-						if (pm[move].x === allSnakes[snake].body.data[0].x - 1 && pm[move].y === allSnakes[snake].body.data[0].y) {
-							pm[move].valid = false;
-						}
-						if (pm[move].x === allSnakes[snake].body.data[0].x && pm[move].y === allSnakes[snake].body.data[0].y + 1) {
-							pm[move].valid = false;
-						}
-						if (pm[move].x === allSnakes[snake].body.data[0].x && pm[move].y === allSnakes[snake].body.data[0].y - 1) {
-							pm[move].valid = false;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	checkSelf(gameState, possibleMoves);
-	checkEdges(gameState, possibleMoves);
-	checkSnakes(gameState, possibleMoves);
-
-	var validMoves = [];
-	for (var i in possibleMoves) {
-		if (possibleMoves[i].valid) {
-			validMoves.push(possibleMoves[i]);
-		}
-	}
-
-	// if no spaces are safe, this will allow to move into spaces bigger snakes can allow move into
-	if (!validMoves.length) {
-		// console.log('NO PATH, NO OPEN MOVES');
-
-		//Reset possibleMoves
-		for (var i in possibleMoves) {
-			possibleMoves[i].valid = true
-		}
-
-		//Recheck possibleMoves but ignoring larger snakes
-		gameState.you.length += 100;
-		checkSelf(gameState, possibleMoves);
-		checkEdges(gameState, possibleMoves);
-		checkSnakes(gameState, possibleMoves);
-		for (var i in possibleMoves) {
-			if (possibleMoves[i].valid) {
-				validMoves.push(possibleMoves[i]);
-			}
-		}
-	}
-
-	snakeResponse.move = validMoves[0].direction;
-	snakeResponse.taunt = taunts[1];
-	console.log(snakeResponse);
-
-	// return res.json(snakeResponse);
-
-} else {
-	function setMove(path, head) {
-		if (path[1][0] === head.x && path[1][1] === head.y + 1) {
-			return 'down';
-		} else if (path[1][0] === head.x && path[1][1] === head.y - 1) {
-			return 'up';
-		} else if (path[1][0] === head.x + 1 && path[1][1] === head.y) {
-			return 'right';
-		} else if (path[1][0] === head.x - 1 && path[1][1] === head.y) {
-			return 'left';
-		} else {
-			return 'up';
-		}
-	}
-
-	snakeResponse.move = setMove(path, myHead);
-	snakeResponse.taunt = taunts[getTaunt(gameState)];
-	console.log(snakeResponse);
-	// return res.json(snakeResponse);
-
 }
